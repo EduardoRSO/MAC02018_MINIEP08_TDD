d70a98532e220ed580e9216a9c212fa033b7bd28
Eduardo Ribeiro Silva de Oliveira <edusillva784@gmail.com>
2022-06-07 12:36:08 -0300

    Add test to verify the number of odd/even numbers on the 2D array

--- magicSquare.cpp
+++ magicSquare.cpp
@@ -12,11 +12,40 @@ bool allEqual(int N, int **m){
 	
 }
 
+int **see2D(int N, int **m){
+	for(int i = 0; i < N; i++){
+		cout << "[ ";
+		for(int j = 0; j < N; j++)
+			cout << m[i][j] << " ";
+		cout << "]\n";
+	}
+	return m;
+}
+
+bool equalEvenAndOddNumbers(int N, int **m){
+	int horizontal = 0, vertical = 0, diagonal = 0;
+	for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if(i == 0 and (m[i][j] % 2 == 0)) horizontal+=1;
+			if(j == 0 and (m[i][j] % 2 == 0)) vertical+=1;
+			if(i == j and (m[i][j] % 2 == 0)) diagonal+=1;
+		}
+	}
+	if(horizontal == diagonal == vertical) return 1;
+	return 0;
+}	
+
+
+
 bool magicSquare(int N, int **m){
+	see2D(N, m);
 	if (N == 1){
 		return 1;
 	}
 	if(allEqual(N, m)){
+		return 1;
+	}
+	if(equalEvenAndOddNumbers(N, m)){
 		return 1;
 	}
 
@@ -35,16 +64,6 @@ int **fill2D(int N, int **m){
 	for(int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
 			cin >> m[i][j];
-	return m;
-}
-
-int **see2D(int N, int **m){
-	for(int i = 0; i < N; i++){
-		cout << "[ ";
-		for(int j = 0; j < N; j++)
-			cout << m[i][j] << " ";
-		cout << "]\n";
-	}
 	return m;
 }
 
@@ -87,10 +106,27 @@ void test3(){
 	m[1][0] = 1;
 	m[1][1] = 2;
 	
-	if(magicSquare(N, m) == true){
+	if(!magicSquare(N, m) == true){
 		cout << "Test 3: OK\n";
 	}else{
 		cout << " Test 3: NO\n";	
+	}
+
+}
+
+void test4(){
+	int N = 2;
+	int **m = create2D(N);
+	
+	m[0][0] = 1;
+	m[0][1] = 2;
+	m[1][0] = 4;
+	m[1][1] = 2;
+	
+	if(magicSquare(N, m) == true){
+		cout << "Test 4: OK\n";
+	}else{
+		cout << " Test 4: NO\n";	
 	}
 
 }
@@ -104,5 +140,6 @@ int main(){
 	test1();
 	test2();
 	test3();
+	test4();
 	return 0;
 }
