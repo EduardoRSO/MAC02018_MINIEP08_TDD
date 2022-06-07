94cb4914454b9417ea4a6537a52f3ed54cf27dd6
Eduardo Ribeiro Silva de Oliveira <edusillva784@gmail.com>
2022-06-07 11:34:45 -0300

    Add function to create 2D array as a pointer to pointer

--- magicSquare.cpp
+++ magicSquare.cpp
@@ -2,20 +2,32 @@
 
 using namespace std;
 
-bool magicSquare(int m[][1]){
+bool magicSquare(int N, int **m){
+	if (N == 1){
+		return 1;
+	}
 	return 0;
 }
 
-void test1(int m[][1]){
-	if(magicSquare(m) == true){
-		cout << " Test 1: OK\n";
+void test1(int N, int **m){
+	if(magicSquare(N, m) == true){
+		cout << "Test 1: OK\n";
 	}else{
 		cout << " Test 1: NO\n";	
 	}
 }
 
+int **create2D(int N){
+	int **temp = new int *[N];
+	for(int i = 0; i < N; i++)
+		temp[i] = new int[N];
+	return temp;
+}
+
 int main(){
-	int array[1][1] = {1};
-	test1(array);
+	int N = 1;
+	int **array = create2D(N);
+	array[0][0] = 1;
+	test1(N, array);
 	return 0;
 }
